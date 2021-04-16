2be5a4bdb2fc115c795b41cb8cd1a01685a1a753
takashi oka <takashi.oka@gmail.com>
2021-04-16 14:31:09 +0900

    user.rb modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-  has_many :pictures
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  mount_uploader :image, ImageUploader
+
+  has_many :pictures
 end
 
